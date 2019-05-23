3e97260326adf03360f3ab0761c7613f695aba59
Yuri Victor <yurivictor@gmail.com>
2019-05-23 15:14:49 -0700

    adding emoji struct

---
+++ quick-emoji/Emoji.swift
@@ -0,0 +1,15 @@
+//
+//  Emoji.swift
+//  quick-emoji
+//
+//  Created by Victor, Yuri on 5/23/19.
+//  Copyright © 2019 Victor, Yuri. All rights reserved.
+//
+
+import Foundation
+
+struct Emoji {
+    let category : String
+    let name : String
+    let icon : String
+}
